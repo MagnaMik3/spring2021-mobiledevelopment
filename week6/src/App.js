9a002e0e26619506afdea68f70db48ea9ea15469
MagnaMik3 <75513195+MagnaMik3@users.noreply.github.com>
2021-02-19 23:07:13 -0500

    Added week 6 assignment

---
+++ week6/src/App.js
@@ -0,0 +1,9 @@
+import Example from './components/example/Index';
+
+function App() {
+  return (
+     <Example/>
+  );
+}
+
+export default App;
---
+++ week6/src/components/example/Index.js
@@ -0,0 +1,8 @@
+import react from 'react';
+class Example extends React.Component {
+    render() {
+        return<p>Hello, my name is Steven Capute!</p>;
+    }
+}
+
+export default Example;
