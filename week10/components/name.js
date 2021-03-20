38b8abcd84896a1215a948bc560d4fc7260a411e
MagnaMik3 <75513195+MagnaMik3@users.noreply.github.com>
2021-03-19 23:47:58 -0400

    Week10

---
+++ week10/components/name.js
@@ -0,0 +1,12 @@
+import React, {component} from "React";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Name extends Component {
+    render() {
+        return(
+           <View>
+               <Text>Steven Capute</Text>
+           </View>
+        );
+    }
+}
